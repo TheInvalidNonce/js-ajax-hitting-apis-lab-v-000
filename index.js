789c82264266039ae565cc08e2f76a3cb15965e3
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-15 23:18:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function getRepositories() {
+  let user = document.getElementById("username").value;
+  const req = new XMLHttpRequest();
+  
+  req.addEventListener("load", showRepositories())
+  
+  req.open("GET", `${'https://api.github.com/users/' + user + '/repos'}`)
+  
+  req.send()
+}
+
+function showRepositories(event, data) {
+  let repos = JSON.parse(this.responseText)
+  console.log(repos);
+  
+const repoList = `<ul>${repos.map(r => '<li>' + r.name + r.html_url + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  
+  document.getElementById('repositories').innerHTML = repoList
+}
